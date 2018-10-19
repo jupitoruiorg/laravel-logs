12124bafcbe52bbc671785e9af0ad3faa8f87f7e
Roman Nebesnuy <sky.getcode@gmail.com>
2018-10-19 18:34:12 +0300

    add repository trait

---
+++ src/Traits/RepositoryTrait.php
@@ -0,0 +1,61 @@
+<?php namespace GetCode\LaravelLogs\Traits;
+/**
+ * Created by PhpStorm.
+ * User: romannebesnuy
+ * Date: 19.10.2018
+ * Time: 18:32
+ */
+
+
+trait RepositoryTrait
+{
+    /**
+     * Create a new instance of the model.
+     *
+     * @param  array  $data
+     * @return mixed
+     */
+    public function createModel(array $data = [])
+    {
+        $class = '\\'.ltrim($this->model, '\\');
+
+        return new $class($data);
+    }
+
+    /**
+     * Returns the model.
+     *
+     * @return string
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Runtime override of the model.
+     *
+     * @param  string  $model
+     * @return $this
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    /**
+     * Dynamically pass missing methods to the model.
+     *
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        $model = $this->createModel();
+
+        return call_user_func_array([$model, $method], $parameters);
+    }
+}
